001bc1d20281f9dc4f61fb4052042d828356f694
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-17 08:12:27 +0100

    Add child componet to calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,10 @@
 import './Calculator.css';
+import CalcButtons from './Calcbuttons';
 
 const Calculator = () => (
   <section className="calculator">
     <div className="screen">0</div>
-    <div className="work">
-      <div>AC</div>
-      <div>+/-</div>
-      <div>%</div>
-      <div className="function">/</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-      <div className="function">x</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div className="function">-</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div className="function">+</div>
-      <div className="zero">0</div>
-      <div>.</div>
-      <div className="function">=</div>
-    </div>
+    <CalcButtons />
   </section>
 );
 
