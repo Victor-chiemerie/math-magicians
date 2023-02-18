32d64cc63019285e2fee75a7ebef4ed677884f9b
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-18 11:15:00 +0100

    Update calulator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 import './Calculator.css';
 import React, { useState } from 'react';
 import Calculate from '../logic/calculate';
+import Btn from './butoons';
 
 const Calculator = () => {
   const [state, setState] = useState({
@@ -23,25 +24,25 @@ const Calculator = () => {
         {state.next || state.operation || state.total || 0}
       </div>
       <div className="work">
-        <button type="button" id="AC" onClick={handleClick}>AC</button>
-        <button type="button" id="+/-" onClick={handleClick}>+/-</button>
-        <button type="button" id="%" onClick={handleClick}>%</button>
-        <button type="button" id="÷" className="function" onClick={handleClick}>÷</button>
-        <button type="button" id="7" onClick={handleClick}>7</button>
-        <button type="button" id="8" onClick={handleClick}>8</button>
-        <button type="button" id="9" onClick={handleClick}>9</button>
-        <button type="button" id="x" className="function" onClick={handleClick}>x</button>
-        <button type="button" id="4" onClick={handleClick}>4</button>
-        <button type="button" id="5" onClick={handleClick}>5</button>
-        <button type="button" id="6" onClick={handleClick}>6</button>
-        <button type="button" id="-" className="function" onClick={handleClick}>-</button>
-        <button type="button" id="1" onClick={handleClick}>1</button>
-        <button type="button" id="2" onClick={handleClick}>2</button>
-        <button type="button" id="3" onClick={handleClick}>3</button>
-        <button type="button" id="+" className="function" onClick={handleClick}>+</button>
-        <button type="button" id="0" className="zero" onClick={handleClick}>0</button>
-        <button type="button" id="." onClick={handleClick}>.</button>
-        <button type="button" id="=" className="function" onClick={handleClick}>=</button>
+        <Btn id="AC" handleClick={handleClick} />
+        <Btn id="+/-" handleClick={handleClick} />
+        <Btn id="%" handleClick={handleClick} />
+        <Btn id="÷" handleClick={handleClick} styled="function" />
+        <Btn id="7" handleClick={handleClick} />
+        <Btn id="8" handleClick={handleClick} />
+        <Btn id="9" handleClick={handleClick} />
+        <Btn id="x" handleClick={handleClick} styled="function" />
+        <Btn id="4" handleClick={handleClick} />
+        <Btn id="5" handleClick={handleClick} />
+        <Btn id="6" handleClick={handleClick} />
+        <Btn id="-" handleClick={handleClick} styled="function" />
+        <Btn id="1" handleClick={handleClick} />
+        <Btn id="2" handleClick={handleClick} />
+        <Btn id="3" handleClick={handleClick} />
+        <Btn id="+" handleClick={handleClick} styled="function" />
+        <Btn id="0" handleClick={handleClick} styled="zero" />
+        <Btn id="." handleClick={handleClick} />
+        <Btn id="=" handleClick={handleClick} styled="function" />
       </div>
     </section>
   );
