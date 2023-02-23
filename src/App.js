068abea2b70f35bba73b2f4f30cf11b3f561b5ce
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-23 15:38:33 +0100

    Setup home, calculator and quote pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './navBar/navBar';
 import Calculator from './components/Calculator';
+import Home from './home/home';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <Calculator />
+    <Router>
+      <div className="app" />
+      <NavBar />
+      <div className="content">
+        <Routes>
+          <Route path="/c" element={<Home />} />
+          <Route path="/c" element={<Calculator />} />
+          <Route path="/" element={<Quote />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,31 +19,34 @@ const Calculator = () => {
   };
 
   return (
-    <section className="calculator">
-      <div className="screen">
-        {state.next || state.operation || state.total || 0}
-      </div>
-      <div className="work">
-        <Btn id="AC" handleClick={handleClick} />
-        <Btn id="+/-" handleClick={handleClick} />
-        <Btn id="%" handleClick={handleClick} />
-        <Btn id="÷" handleClick={handleClick} styled="function" />
-        <Btn id="7" handleClick={handleClick} />
-        <Btn id="8" handleClick={handleClick} />
-        <Btn id="9" handleClick={handleClick} />
-        <Btn id="x" handleClick={handleClick} styled="function" />
-        <Btn id="4" handleClick={handleClick} />
-        <Btn id="5" handleClick={handleClick} />
-        <Btn id="6" handleClick={handleClick} />
-        <Btn id="-" handleClick={handleClick} styled="function" />
-        <Btn id="1" handleClick={handleClick} />
-        <Btn id="2" handleClick={handleClick} />
-        <Btn id="3" handleClick={handleClick} />
-        <Btn id="+" handleClick={handleClick} styled="function" />
-        <Btn id="0" handleClick={handleClick} styled="zero" />
-        <Btn id="." handleClick={handleClick} />
-        <Btn id="=" handleClick={handleClick} styled="function" />
-      </div>
+    <section>
+      <div className="topic">Let’s do some math</div>
+      <main className="calculator">
+        <div className="screen">
+          {state.next || state.operation || state.total || 0}
+        </div>
+        <div className="work">
+          <Btn id="AC" handleClick={handleClick} />
+          <Btn id="+/-" handleClick={handleClick} />
+          <Btn id="%" handleClick={handleClick} />
+          <Btn id="÷" handleClick={handleClick} styled="function" />
+          <Btn id="7" handleClick={handleClick} />
+          <Btn id="8" handleClick={handleClick} />
+          <Btn id="9" handleClick={handleClick} />
+          <Btn id="x" handleClick={handleClick} styled="function" />
+          <Btn id="4" handleClick={handleClick} />
+          <Btn id="5" handleClick={handleClick} />
+          <Btn id="6" handleClick={handleClick} />
+          <Btn id="-" handleClick={handleClick} styled="function" />
+          <Btn id="1" handleClick={handleClick} />
+          <Btn id="2" handleClick={handleClick} />
+          <Btn id="3" handleClick={handleClick} />
+          <Btn id="+" handleClick={handleClick} styled="function" />
+          <Btn id="0" handleClick={handleClick} styled="zero" />
+          <Btn id="." handleClick={handleClick} />
+          <Btn id="=" handleClick={handleClick} styled="function" />
+        </div>
+      </main>
     </section>
   );
 };
