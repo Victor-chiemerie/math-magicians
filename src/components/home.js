44eb8c0db8dd96f44b93bbda620843ad5cd5f386
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-02-23 20:46:17 +0100

    Fix linters error on all files

--- src/components/home.js
+++ src/components/home.js
@@ -4,25 +4,31 @@ const Home = () => (
   <div className={styles.home}>
     <h2>Welcome to Victor’s Calculator Web-App</h2>
     <p>
-      President Muhammadu Buhari on Thursday said the sudden drop in
-      the price of crude oil has left Nigeria poor,
-      leaving it to struggle economically and it people suffering.
-      Mr. Buhari however said his administration’s commitment to transparency and accountability
-      was serving the country in good stead, despite severe shortage of resources in the country.
-      The President spoke Thursday at the State House, Abuja,
-      while receiving the United Nations Population Fund (UNFPA)
-      Executive Director and Under Secretary General of
-      the United Nations, Prof Babatunde Osotimehin.
-      “It has been a very difficult year for Nigeria. Before we came to office,
-      petroleum sold for about $100 per barrel.
-      Then it crashed to $37, and now oscillates between $40 and $45 per barrel.
-      Suddenly, we’re a poor country, but commitment to transparency and
-      accountability is not making people know that there is severe shortage,”
-      Special Adviser on Media and Publicity, Femi Adesina, quoted the President as saying.
-      Asking UNFPA to bear with Nigeria in whichever area the country
-      could not live up to its responsibilities for now,
-      President Buhari said exploding population and different cultural
-      practices in the country provide fertile ground for research to organizations like UNFPA
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
+        vestibulum erat. Nulla sagittis massa at egestas facilisis. Quisque
+        tincidunt interdum vulputate. Sed orci orci, viverra vel tincidunt vel,
+        consectetur eget tortor. Integer pellentesque eros vel ex maximus, quis
+        aliquet nisl tristique. Nulla lobortis sem a placerat maximus. Duis
+        suscipit purus at nunc bibendum, et finibus quam tempor. Donec sed magna
+        suscipit, bibendum nisi faucibus, dictum erat. Aenean varius eros vitae
+        arcu tempus lobortis. Ut et nisl eget purus ornare tincidunt. Aliquam erat
+        volutpat. Suspendisse vitae finibus odio. Suspendisse aliquam risus massa,
+        vel vulputate mauris tempus a.
+      </span>
+      <br />
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
+        vestibulum erat. Nulla sagittis massa at egestas facilisis. Quisque
+        tincidunt interdum vulputate. Sed orci orci, viverra vel tincidunt vel,
+        consectetur eget tortor. Integer pellentesque eros vel ex maximus, quis
+        aliquet nisl tristique. Nulla lobortis sem a placerat maximus. Duis
+        suscipit purus at nunc bibendum, et finibus quam tempor. Donec sed magna
+        suscipit, bibendum nisi faucibus, dictum erat. Aenean varius eros vitae
+        arcu tempus lobortis. Ut et nisl eget purus ornare tincidunt. Aliquam erat
+        volutpat. Suspendisse vitae finibus odio. Suspendisse aliquam risus massa,
+        vel vulputate mauris tempus a.
+      </span>
     </p>
   </div>
 );
--- src/components/notMatch.js
+++ src/components/notMatch.js
@@ -1,8 +1,9 @@
 import styles from '../styles/notMatch.module.scss';
 
 const NotMatch = () => (
-  <div className={styles.div}>
-    <p>Not found!</p>
+  <div className={styles.error}>
+    <h1>404</h1>
+    <p>Page not found!</p>
   </div>
 );
 
